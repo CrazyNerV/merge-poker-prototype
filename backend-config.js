eda8e82d9090b90355fc39f029189610e365ab27
CrazyNerV <nerv.neowiz@gmail.com>
2026-08-26 00:10:59 +0900

    Connect Supabase global leaderboard

--- backend-config.js
+++ backend-config.js
@@ -1,4 +1,4 @@
 window.MERGE_POKER_BACKEND = {
-  url: '',
-  publishableKey: ''
+  url: 'https://uiugqbkismerfqhmgqqv.supabase.co',
+  publishableKey: 'sb_publishable_1jxS9DH1hFSriJPC6C_3sQ_vbzR9Hjt'
 };
